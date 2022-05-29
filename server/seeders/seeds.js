4e664d620c18d50b9ec325c2fadbbd76af121ea7
Tyler Beck <tylerbeck@Tylers-Air.attlocal.net>
2022-05-28 20:00:28 -0700

    new

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,17 +1,17 @@
 const userSeeds = require('./userSeed.json');
 const recipeSeeds = require('./recipeSeed.json');
 const db = require('../config/connection');
-const { recipe, User } = require('../models');
+const { Recipe, User } = require('../models');
 
 db.once('open', async () => {
   try {
-    await recipe.deleteMany({});
+    await Recipe.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
     for (let i = 0; i < recipeSeeds.length; i++) {
-      const { _id, recipeAuthor } = await recipe.create(recipeSeeds[i]);
+      const { _id, recipeAuthor } = await Recipe.create(recipeSeeds[i]);
       const user = await User.findOneAndUpdate(
         { username: recipeAuthor },
         {
